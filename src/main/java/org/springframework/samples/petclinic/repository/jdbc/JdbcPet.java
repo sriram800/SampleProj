6a71ec988c518083f0ecd1af63f010e37e361733
sriramulu.parisa <sriramulu.parisa@kony.com>
2014-12-15 17:14:53 +0530

    Change

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPet.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPet.java
@@ -17,10 +17,22 @@ package org.springframework.samples.petclinic.repository.jdbc;
 
 import org.springframework.samples.petclinic.model.Pet;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Vets;
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
 
 
 /**
