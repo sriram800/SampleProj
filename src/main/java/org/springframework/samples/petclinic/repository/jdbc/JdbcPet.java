1363bb140943016ab5cb7365b9a622592bb577a2
sriramulu.parisa <sriramulu.parisa@kony.com>
2014-12-16 22:14:09 +0530

    Test

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPet.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPet.java
@@ -18,7 +18,6 @@ package org.springframework.samples.petclinic.repository.jdbc;
 import org.springframework.samples.petclinic.model.Pet;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 
 
