1949e47a59d1cff286f676b9d4fd2b99aa8dc87e
sriramulu.parisa <sriramulu.parisa@kony.com>
2014-12-16 23:10:18 +0530

    Test

---
+++ src/main/java/org/springframework/samples/petclinic/model/Device.java
@@ -0,0 +1,41 @@
+package org.springframework.samples.petclinic.model;
+
+import java.io.Serializable;
+
+public class Device implements Serializable{
+	
+	private String deviceId;
+	
+	private String code;
+
+	/**
+	 * @return the deviceId
+	 */
+	public String getDeviceId() {
+		return deviceId;
+	}
+
+	/**
+	 * @param deviceId the deviceId to set
+	 */
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+
+}
