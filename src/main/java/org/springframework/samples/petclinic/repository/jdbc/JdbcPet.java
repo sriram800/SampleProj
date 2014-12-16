fe9a8a784b8ea5ab3717b748cf32c84e98e4f3ea
sriramulu.parisa <sriramulu.parisa@kony.com>
2014-12-16 14:59:11 +0530

    Test

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPet.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPet.java
@@ -25,8 +25,6 @@ import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Owner;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.stereotype.Controller;
 
@@ -45,7 +43,6 @@ class JdbcPet extends Pet {
 
     private int ownerId;
     
-    private String name;
     
     public String getName() {
 		return name;
