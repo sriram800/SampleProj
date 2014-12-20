eadc89de543b8b93923b60e9b95d600f38786994
sriramulu.parisa <sriramulu.parisa@kony.com>
2014-12-20 16:21:30 +0530

    Test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -61,7 +61,24 @@ public class ValidatorTests {
 
         Assert.assertEquals(1, constraintViolations.size());
         ConstraintViolation<Person> violation =  constraintViolations.iterator().next();
-        Assert.assertEquals(violation.getPropertyPath().toString(), "firstName1");
+        Assert.assertEquals(violation.getPropertyPath().toString(), "firstName");
+        Assert.assertEquals(violation.getMessage(), "may not be empty");
+    }
+	
+	@Test
+    public void emptyFirstName2() {
+
+	LocaleContextHolder.setLocale(Locale.ENGLISH);
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("smith");
+
+        Validator validator = createValidator();
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+
+        Assert.assertEquals(1, constraintViolations.size());
+        ConstraintViolation<Person> violation =  constraintViolations.iterator().next();
+        Assert.assertEquals(violation.getPropertyPath().toString(), "firstName");
         Assert.assertEquals(violation.getMessage(), "may not be empty");
     }
 	
