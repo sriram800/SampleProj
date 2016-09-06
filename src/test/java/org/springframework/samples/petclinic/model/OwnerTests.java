5fbc62acf1afca6cdcd9ca475fd242548fe2d486
Sriramulu Parisa <sriramulu.parisa@kony.com>
2016-09-06 16:57:30 +0530

    Test added

--- src/test/java/org/springframework/samples/petclinic/model/OwnerTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/OwnerTests.java
@@ -147,5 +147,18 @@ public class OwnerTests {
         assertEquals(fido, owner.getPet("Fido1"));
         assertEquals(fido, owner.getPet("fido1"));
     }
+    
+    @Test
+    @Transactional
+    public void testHasPet63() {
+        Owner owner = new Owner();
+        Pet fido = new Pet();
+        fido.setName("Fido1");
+        assertNull(owner.getPet("Fido1"));
+        assertNull(owner.getPet("fido1"));
+        owner.addPet(fido);
+        assertEquals(fido, owner.getPet("Fido1"));
+        assertEquals(fido, owner.getPet("fido1"));
+    }
 
 }
